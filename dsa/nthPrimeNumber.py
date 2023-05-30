f9dc91221a92f67ae123c503b589bff39436f2e0
Dhiraj Singh <dhirajkr1117@gmail.com>
2023-05-31 02:08:04 +0530

    n(numberofPrime).py

---
+++ dsa/nthPrimeNumber.py
@@ -0,0 +1,21 @@
+def printPrime(n):
+    x=2
+    num=str(n)
+    count=0
+    arr=[]
+    while len(arr)<n:
+        flag=True
+        for i in range(2,x):
+            if(x%i)==0:
+                flag=False
+        if flag==True:
+            arr.append(x)
+            count+=1
+        if count==n:
+            break
+        x+=1
+    return arr
+def main():
+    n=int(input())
+    print(printPrime(n))
+main()
