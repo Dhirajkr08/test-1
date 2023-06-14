6f0e1318f9cd71d190ed96b0cdcc12d9e525a9b0
Dhiraj Singh <dhirajkr1117@gmail.com>
2023-06-14 21:26:05 +0530

    call booking Appointment

---
+++ dsa/wdq6.js
@@ -0,0 +1,11 @@
+function onsignup(event){
+    event.preventDefault();
+    var name=document.getElementById('name').value;
+    var email=document.getElementById('email').value;
+    var phone=document.getElementById('phone').value;
+    var date=document.getElementById('dateTime').value;
+    console.log(name)
+    console.log(email)
+    console.log(phone)
+    console.log(date)
+}
