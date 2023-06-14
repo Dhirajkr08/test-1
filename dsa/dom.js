ab0926f97ef5a3ba3d0040eb637356e019d3cdb9
Dhiraj Singh <dhirajkr1117@gmail.com>
2023-06-14 21:21:08 +0530

    dom practise

---
+++ dsa/dom.js
@@ -0,0 +1,31 @@
+//console.dir(document) //for excessing the the whole document
+//console.log(document.domain);
+//console.log(document.URL);
+//console.log(document.title);
+//document.title=123;//for changing the title from here//
+
+//console.log(document.doctype);//grab doctype
+//console.log(document.head);//grab head element
+
+//console.log(document.body);
+//console.log(document.all);
+//console.log(document.all[10]);//for grabbing with indexes
+
+//console.log(document.forms);
+//console.log(document.links);
+//console.log(document.images);
+
+//GETELEMENTBYID//
+//var headerTitle=document.getElementById('header-title');
+//console.log(headerTitle)
+//headerTitle.textContent="hello";
+//headerTitle.textContent="hello";
+//headerTitle.innerText="Goodbye";
+//headerTitle.innerText="GoodBye";
+//headerTitle.innerHTML='<h2>Hello</h2>';
+//ar header=document.getElementById('mainHeader');
+//console.log(header)
+//header.style.backgroundColor='red';
+var header=document.getElementById('title');
+header.style.fontColor='green';
+header.style.fontWeight='bold';
