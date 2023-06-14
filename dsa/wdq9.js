80477d8e57221026247249c787f755c00b7044a6
Dhiraj Singh <dhirajkr1117@gmail.com>
2023-06-14 21:46:25 +0530

    javaScript personal Details

---
+++ dsa/wdq9.js
@@ -0,0 +1,6 @@
+var name= "Dhiraj Singh";
+var age= 25;
+var address="Boring Road Patna";
+
+var personalDetails="my name is" +name+", age is"+age+", address is" +address+".";
+console.log(personalDetails);
